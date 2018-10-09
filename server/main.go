7175f19dcb93217bc502f49d938d660a09be037b
Taichi Hagiwara <26000425+taichi-hagiwara@users.noreply.github.com>
2018-10-10 01:36:07 +0900

    :black_nib: ezrpc の変更に合わせて変更

--- server/main.go
+++ server/main.go
@@ -10,11 +10,12 @@ import (
 	"github.com/taichi-hagiwara/ezrpc"
 )
 
+var logs = make(map[string][]*service.Post)
+
 func main() {
-	if err := ezrpc.Serve(&chatServer{
-		Service: service.ChatService(),
-		Log:     make(map[string][]*service.Post),
-	}, "localhost:8080", &ezrpc.CertInfo{
+	server := ezrpc.NewServer(service.ChatService())
+	server.RegisterHandler("post", postHandler)
+	if err := server.Listen("localhost:8080", &ezrpc.CertInfo{
 		CACert:  "ca.pem",
 		Cert:    "cert.pem",
 		Private: "private.pem",
@@ -23,29 +24,19 @@ func main() {
 	}
 }
 
-type chatServer struct {
-	ezrpc.Service
+func postHandler(client *ezrpc.ClientInfo, args interface{}) (result interface{}, err error) {
+	a := args.(*service.PostArgs)
+	post := &service.Post{Text: a.Text, Name: client.TLSSubject.CommonName, Time: time.Now()}
+	fmt.Printf("%s %s <%s> %s\n", client.Remote, post.Time, post.Name, post.Text)
 
-	Log map[string][]*service.Post
-}
-
-func (s *chatServer) Invoke(name string, client *ezrpc.ClientInfo, args interface{}) (result interface{}, err error) {
-	if name == "post" {
-		args := args.(*service.PostArgs)
-		post := &service.Post{Text: args.Text, Name: client.TLSSubject.CommonName, Time: time.Now()}
-		fmt.Printf("%s %s <%s> %s\n", client.Remote, post.Time, post.Name, post.Text)
-
-		if _, ok := s.Log[client.TLSSubject.CommonName]; !ok {
-			s.Log[client.TLSSubject.CommonName] = []*service.Post{}
-		}
-		for i := range s.Log {
-			s.Log[i] = append(s.Log[i], post)
-		}
-
-		log := s.Log[client.TLSSubject.CommonName]
-		s.Log[client.TLSSubject.CommonName] = []*service.Post{}
-		return &service.PostResult{Log: log}, nil
-
+	if _, ok := logs[client.TLSSubject.CommonName]; !ok {
+		logs[client.TLSSubject.CommonName] = []*service.Post{}
 	}
-	return nil, nil
+	for i := range logs {
+		logs[i] = append(logs[i], post)
+	}
+
+	log := logs[client.TLSSubject.CommonName]
+	logs[client.TLSSubject.CommonName] = []*service.Post{}
+	return &service.PostResult{Log: log}, nil
 }
